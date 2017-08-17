4bedd012560c16d471452e3a1bfe5d631081d973
Shaunna <skwiens@gmail.com>
2017-08-17 16:59:12 -0700

    [JE/SW] Completed Card Class basic methods, still needing loops and user entry verification, guess count termination

--- word_guess.rb
+++ word_guess.rb
@@ -4,11 +4,11 @@ class Game
   attr_reader :level
 
   def initialize
-    @word_bank = %W[Mercury Venus Mars Earth]
+    @word_bank = %W[MERCURY VENUS MARS EARTH]
   end
 
   def get_level
-    puts "Let's get started! You have access to three levels: Easy, Medium, and Hard. \n\nNOTE: Easy gives you 10 guesses; Medium gives you 5 guesses, and Hard gives you only 3 guesses. Ready to play?"
+    puts "Let's get started! You have access to three levels: Easy, Medium, and Hard. \n\nNOTE: Easy gives you 5 missed guesses; Medium gives you 4 guesses, and Hard gives you only 3 guesses. Ready to play?"
 
     puts "Please enter your level choice below:"
 
@@ -26,11 +26,12 @@ class Card
   attr_reader :word, :word_showing
 
   def initialize(word)
-    @word = word
+    @word = word.upcase
     @guess = nil #guess is the variable to store user input
     @guess_type = "invalid"
     @word_showing = build_initial_word_array.join
-    @guess_count = 0
+    @missed_count = 0
+    @word_array = @word.split("") #Check at end if this is used more than in the guess method... if not... delete!
   end
 
   def build_initial_word_array
@@ -46,32 +47,42 @@ class Card
     @guess = gets.chomp
   end
 
-   #guess types: actual word, a letter, or invalid(guessing too many letters, guessing a number)
+  #guess types: actual word, a letter, or invalid(guessing too many letters, guessing a number)
   def check_guess
-    if @guess.length.to_i == @word.length.to_i #if user is guessing word
-      if @guess == @word
+    if @guess.length.to_i == @word.length.to_i #IF USER GUESSES A WORD
+      if @guess.upcase == @word
         print "YAY! CONGRATS! #{@word.upcase} IS CORRECT!"
       else
         print "Sorry, that was incorrect!"
+        @missed_count += 1
+      end
 
+
+    elsif @guess.length == 1 #IF USER GUESSES A LETTER
+      if @word.include?(@guess) #if guessed letter exists in generated word array, iterate over word array to check if letter matches
+        @word_array.each do |element|
+          if element == @guess.upcase#letter
+            letter_index = @word_array.index(element)
+            @word_showing[letter_index] = element #resetting "__" to element which is the guessed letter
+          end
+        end#of word_array loop
+      else #if guessed letter does not exist, then increment guess count
+        @missed_count += 1
       end
     end
+  end #of class Card
+end
 
+  class AsciiArt
+
+    def initialize
     end
-  end
 
-end #of class Card
+    def level_easy_initial
+      puts ""
+    end
 
-class AsciiArt
-
-  def initialize
-  end
-
-  def level_easy_initial
-    puts ""
-  end
-
-end #of class AsciiArt
+  end #of class AsciiArt
 
 
 #START OF GAME____________
@@ -82,12 +93,17 @@ Welcome to Word Guess! You will be shown a series of blanks.  Each blank represe
 
 YES
 
-
 puts introduction
 
 game1 = Game.new
-game1.get_level #card may need to know level type
+# game1.get_level #card may need to know level type
 card1 = Card.new(game1.generate_word)
+puts card1.word
 puts card1.word_showing
+card1.get_guess
+card1.check_guess
+puts card1.word_showing
+
+
 
 # This card1.word will read the card's word
