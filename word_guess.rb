161faa9abfc810e9b40c20c5c0a596b3c72bce98
Shaunna <skwiens@gmail.com>
2017-08-17 14:23:55 -0700

    [JE/SW] made initial classes

--- word_guess.rb
+++ word_guess.rb
@@ -1,1 +1,40 @@
 #This is the word-guess project
+
+class Game
+  attr_reader :level
+
+  def initialize
+
+  end
+
+  def level
+
+end #of class Game
+
+#
+class Card
+
+  def initialize(word)
+    @word = word
+    @guess = nil
+
+  end
+
+  def get_guess
+    puts "What would you like to guess?"
+    @guess = gets.chomp
+  end
+
+  def verify_guess
+    if @guess.length.to_i == @word.length.to_i
+    end
+  end
+
+end #of class Card
+
+class AsciiArt
+
+  def initialize
+  end
+
+end #of class AsciiArt
