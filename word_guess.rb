300a7b3069de43724dcbfb7c85530762bd71e073
Shaunna <skwiens@gmail.com>
2017-08-17 18:34:24 -0700

    [JE/SW] We did a lot! Now it's late and we're going home. Tomorrow: handle missed & invalid guesses and control flow + ascii artgit add word_guess.rb

--- word_guess.rb
+++ word_guess.rb
@@ -1,18 +1,33 @@
 #This is the word-guess project
 
 class Game
-  attr_reader :level
+  attr_reader :max_missed_guesses
 
   def initialize
     @word_bank = %W[MERCURY VENUS MARS EARTH]
+    get_level_and_max_misses
   end
 
-  def get_level
-    puts "Let's get started! You have access to three levels: Easy, Medium, and Hard. \n\nNOTE: Easy gives you 5 missed guesses; Medium gives you 4 guesses, and Hard gives you only 3 guesses. Ready to play?"
+  def get_level_and_max_misses
+    puts "You have access to three levels: Easy, Medium, and Hard. \n\nNOTE: \n* Easy gives you 5 missed guesses \n* Medium gives you 4 guesses\n* Hard gives you only 3 guesses. \n\nReady to play?"
 
-    puts "Please enter your level choice below:"
+    puts "\nPlease enter your level choice below:"
 
-    level_choice = gets.chomp
+    level_choice = gets.chomp.downcase
+
+    case level_choice
+      when "easy"
+        @max_missed_guesses = 5
+      when "medium"
+        @max_missed_guesses = 4
+      when "hard"
+        @max_missed_guesses = 3
+      else
+        puts "I'm sorry. I didn't understand. "
+        get_level_and_max_misses
+    end
+
+
   end
 
   def generate_word
@@ -23,28 +38,28 @@ end #of class Game
 
 class Card
 
-  attr_reader :word, :word_showing
+  attr_reader :word, :word_showing, :missed_count
 
   def initialize(word)
     @word = word.upcase
     @guess = nil #guess is the variable to store user input
     @guess_type = "invalid"
-    @word_showing = build_initial_word_array.join
+    @word_showing = build_blank_word_array
     @missed_count = 0
     @word_array = @word.split("") #Check at end if this is used more than in the guess method... if not... delete!
   end
 
-  def build_initial_word_array
+  def build_blank_word_array
     array = []
     @word.length.times do
-      array << " ____ "
+      array << " _ "
     end
     return array
   end
 
   def get_guess
     puts "What would you like to guess?"
-    @guess = gets.chomp
+    @guess = gets.chomp.upcase
   end
 
   #guess types: actual word, a letter, or invalid(guessing too many letters, guessing a number)
@@ -60,11 +75,13 @@ class Card
 
     elsif @guess.length == 1 #IF USER GUESSES A LETTER
       if @word.include?(@guess) #if guessed letter exists in generated word array, iterate over word array to check if letter matches
+      puts "YES! It DOES include #{@guess}!"
+        i = 0
         @word_array.each do |element|
-          if element == @guess.upcase#letter
-            letter_index = @word_array.index(element)
-            @word_showing[letter_index] = element #resetting "__" to element which is the guessed letter
+          if @guess == element #letter
+            @word_showing[i] = "#{element} " #resetting "__" to element which is the guessed letter
           end
+          i += 1
         end#of word_array loop
       else #if guessed letter does not exist, then increment guess count
         @missed_count += 1
@@ -73,7 +90,7 @@ class Card
   end #of class Card
 end
 
-  class AsciiArt
+class AsciiArt
 
     def initialize
     end
@@ -95,15 +112,24 @@ YES
 
 puts introduction
 
-game1 = Game.new
-# game1.get_level #card may need to know level type
+game1 = Game.new #get user input for level
+
 card1 = Card.new(game1.generate_word)
-puts card1.word
-puts card1.word_showing
-card1.get_guess
-card1.check_guess
-puts card1.word_showing
+
+while true
+  puts card1.word #test: view word
+  puts card1.word_showing.join
+  card1.get_guess
+  card1.check_guess
+  puts card1.missed_count
+  puts card1.word_showing.join
+  puts "Would you like to make another guess?"
+  response = gets.chomp.upcase
+  if response == "NO"
+    exit
+  end
+end
 
 
-
+# level_choice = game1.get_level #card may need to know level type
 # This card1.word will read the card's word
