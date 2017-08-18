22c8a99a7ef35718d2c637d957901cc99222d0be
Shaunna <skwiens@gmail.com>
2017-08-18 15:15:42 -0700

    [JE/SW] Committing check_guess version where it compares @guess.length

--- word_guess.rb
+++ word_guess.rb
@@ -1,41 +1,31 @@
 #This is the word-guess project
 
+require 'terminal-table'
+
 class WordGame
 
-  attr_reader :word, :word_showing, :missed_count, :letters_guessed, :words_guessed
+  attr_reader :word, :word_showing, :letters_guessed, :words_guessed, :level_choice
 
   def initialize
-    @word_bank = %W[APPLE, SANDWICH, PICNIC, BASKET, ANTS, BLANKET, IDYLLIC, MOUNTAINS, LAKE, SUMMMER, FRIENDS, FAMILY]
-    get_level_and_max_misses
-    @word = @word_bank.sample
+    @word_bank = %W[APPLE SANDWICH PICNIC BASKET ANTS BLANKET IDYLLIC MOUNTAINS LAKE SUMMMER FRIENDS FAMILY]
+    @level_choice = get_level
+    @word = @word_bank.sample #generates the word
     @guess = nil #guess is the variable to store user input
-    @guess_type = "invalid"
     @word_showing = build_blank_word_array
-    @missed_count = 0
     @word_array = @word.split("") #Check at end if this is used more than in the guess method... if not... delete!
     @letters_guessed = []
     @words_guessed = []
   end
 
-  def get_level_and_max_misses
+  def get_level
     puts "You have access to three levels: Easy, Medium, and Hard. \n\nNOTE: \n* Easy gives you 5 missed guesses \n* Medium gives you 4 guesses\n* Hard gives you only 3 guesses. \n\nReady to play?"
-
     puts "\nPlease enter your level choice below:"
 
     level_choice = gets.chomp.downcase
-
-    case level_choice
-    when "easy"
-      @max_missed_guesses = 5
-      
-    when "medium"
-      @max_missed_guesses = 4
-    when "hard"
-      @max_missed_guesses = 3
-    else
-      puts "I'm sorry. I didn't understand. "
-      get_level_and_max_misses
+    until level_choice == "easy" || level_choice == "medium" || level_choice == "hard"
+      level_choice = self.get_level
     end
+    return level_choice
   end
 
   def build_blank_word_array
@@ -54,6 +44,7 @@ class WordGame
   #guess types: actual word, a letter, or invalid(guessing too many letters, guessing a number)
   def check_guess
     if @guess.length.to_i == @word.length.to_i #IF USER GUESSES A WORD
+      @words_guessed << @guess 
       if @guess.upcase == @word
         print "YAY! CONGRATS! #{@word.upcase} IS CORRECT!"
       else
@@ -70,8 +61,7 @@ class WordGame
           end
           i += 1
         end#of word_array loop
-      else #if guessed letter does not exist, then increment guess count
-        @missed_count += 1
+      else #if guessed letter does not exist,
       end
     end
   end #of class Card
@@ -102,19 +92,43 @@ puts introduction
 
 game1 = WordGame.new #get user input for level
 
-while true
-  puts game1.word #test: view word
+
+case game1.level_choice
+when "easy"
+  guesses_remaining = 5
+  puts "\e[H\e[2J"
+  puts "\n\n"
+  puts game1.word
   puts game1.word_showing.join
+  puts "guessed letters: #{game1.letters_guessed}"
+  puts "guessed words: #{game1.words_guessed}"
+  puts "remaining guesses: #{guesses_remaining}"
   game1.get_guess
   game1.check_guess
-  puts game1.missed_count
-  puts game1.word_showing.join
-  puts "Would you like to make another guess?"
-  response = gets.chomp.upcase
-  if response == "NO"
-    exit
-  end
+
+when "medium"
+  guesses_remaining = 4
+when "hard"
+  guesses_remaining = 3
+else
+  puts "I'm sorry. I didn't understand. "
+  get_level
 end
+
+# while true
+#   puts "\e[H\e[2J"
+#   puts game1.word #test: view word
+#   puts game1.word_showing.join
+#   game1.get_guess
+#   game1.check_guess
+#   puts game1.missed_count
+#   puts game1.word_showing.join
+#   puts "Would you like to make another guess?"
+#   response = gets.chomp.upcase
+#   if response == "NO"
+#     exit
+#   end
+# end
 
 
 # level_choice = game1.get_level #card may need to know level type
